e13cb8b2598bc485a5c7ef1539329ea56db41b5f
Artem Anisimov <arttimon28@mail.ru>
2022-02-10 22:55:56 +0500

    Скрипт удаления данных базы

---
+++ PairmakerModule/delete_rows.py
@@ -0,0 +1,29 @@
+import os
+import psycopg2
+import configparser
+
+SQL_FILE_NAME = "self_delete_rows.sql"
+
+
+def _connect():
+    conf_name = 'settings.ini'
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), conf_name))
+    params = dict(config['Database'])
+    return psycopg2.connect(**params)
+
+
+if __name__ == "__main__":
+    connection = _connect()
+    cursor = connection.cursor()
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), SQL_FILE_NAME), 'r') as file:
+        commands = file.read()
+        sqlmany = [command + ';' for command in commands.split(';') if len(command) > 5]
+        for sql in sqlmany:
+            try:
+                cursor.execute(sql)
+            except psycopg2.Error as e:
+                pass
+    connection.commit()
+    cursor.close()
+    connection.close()
---
+++ PairmakerModule/self_delete_rows.sql
@@ -0,0 +1,4 @@
+DELETE FROM users;
+DELETE FROM form;
+DELETE FROM identikit;
+DELETE FROM relations;
